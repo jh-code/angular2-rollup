96ad5fd176a1d190532c9116b36309aa243fa0e9
Jared Helgeson <jared@codeninja.guru>
2017-01-12 11:22:32 -0600

    removed unused dev rollup config (for now), formatted rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,29 +2,27 @@ import nodeResolve from 'rollup-plugin-node-resolve';
 import angular from 'rollup-plugin-angular';
 
 class RollupNG2 {
-  resolveId(id, from) {
-    if (id.startsWith('rxjs/')) {
-      return `${__dirname}/node_modules/rxjs-es/${id.replace('rxjs/', '')}.js`;
+    resolveId(id, from) {
+        if (id.startsWith('rxjs/')) {
+            return `${__dirname}/node_modules/rxjs-es/${id.replace('rxjs/', '')}.js`;
+        }
+        return undefined;
     }
-    return undefined;
-  }
 }
 
 const rollupNG2 = () => new RollupNG2();
 
 export default {
-  entry: 'dist/app/main.aot.js',
-  dest: 'dist/bundle.es2015.js',
-  sourceMap: true,
-  treeshake: true,
-  moduleName: 'main',
-  plugins: [
-    rollupNG2(),
-    angular(),
-    nodeResolve({
-      jsnext: true, main: true, module: true
-    })
-  ]
+    entry: 'dist/app/main.aot.js',
+    dest: 'dist/bundle.es2015.js',
+    sourceMap: true,
+    treeshake: true,
+    moduleName: 'main',
+    plugins: [
+        rollupNG2(),
+        angular(),
+        nodeResolve({
+            jsnext: true, main: true, module: true
+        })
+    ]
 };
-
-
